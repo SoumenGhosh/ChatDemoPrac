bc1e1e66500e1da21a436734b13d3ce3e94a9b97
Soumen Ghosh <soumeng007@gmail.com>
2018-03-01 20:20:10 +0600

    final commit

---
+++ ChatDemoPrac/Model/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  ChatDemoPrac
+//
+//  Created by SOUMEN GHOSH on 20/1/18.
+//  Copyright © 2018 Sg106. All rights reserved.
+//
+
+import Foundation
---
+++ ChatDemoPrac/View/ChatCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatCell.swift
+//  ChatDemoPrac
+//
+//  Created by SOUMEN GHOSH on 20/1/18.
+//  Copyright © 2018 Sg106. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+    
+    
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
